7fce28da4ead5b84b708be75ee062fb872c50b9b
Axel <axel.brionne@gmail.com>
2020-08-21 14:54:07 +0200

    Close #4 Permettre de liée des « Cars » à des « Parking »

--- src/Controller/CarController.php
+++ src/Controller/CarController.php
@@ -6,6 +6,7 @@ use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
 use Symfony\Component\Routing\Annotation\Route;
 use App\Entity\Car;
 use App\Form\CarType;
+use App\Entity\Parking;
 use Symfony\Component\HttpFoundation\Request;
 
 class CarController extends AbstractController
@@ -32,5 +33,36 @@ class CarController extends AbstractController
         else {
             return $this->render('car/add.html.twig', array('form' => $form->createView(), 'alert' => false));
         }
+    }
+
+    /**
+     * @Route("/car/link", name="link")
+     */
+    public function linkParking(Request $request)
+    {
+        $repositoryCar = $this->getDoctrine()->getRepository(Car::class);
+        $repositoryParking = $this->getDoctrine()->getRepository(Parking::class);
+        $cars = $repositoryCar->findBy(array('parking' => null));
+        $parkings = $repositoryParking->findAll();
+        if ($request->isXmlHttpRequest()) {  
+            $carName = $_POST['carName'];
+            $parkingName = $_POST['parkingName'];
+            $car = $repositoryCar->findOneBy(array('name' => $carName));
+            if($car !== null){
+            $parking = $repositoryParking->findOneBy(array('name' => $parkingName));
+            if($parking !== null) {
+                $car->setParking($parking);
+                $entityManager = $this->getDoctrine()->getManager();
+                $entityManager->persist($car);
+                $entityManager->flush();
+            } else {
+                $car->setParking(null);
+                $entityManager = $this->getDoctrine()->getManager();
+                $entityManager->persist($car);
+                $entityManager->flush();
+            }
+        }
+        }
+        return $this->render('car/link.html.twig', array('cars' => $cars, 'parkings' => $parkings));
     }
 }
